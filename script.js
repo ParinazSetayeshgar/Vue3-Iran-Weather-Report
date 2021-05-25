fd940e83da5c9963f45f5549b74dd6aee220166a
Parinaz Setayeshgar <p.setayeshgar@yahoo.com>
2021-05-25 15:30:43 +0430

    Calling Openweather API

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+//storing .weather & .temperature elements
+var weatherReport = document.querySelector(".weather");
+var temperatureReport = document.querySelector(".temperature");
+//calling API
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Tehran&appid=59047e5b5fb99d3b3a17ffa14fe1730d')
+    .then(response => response.json()) //creating climate object
+    .then(function(climate) {
+        //showing weather description
+        weatherReport.innerHTML = climate.weather[0].description;
+        function kelvinToCelsius() {
+            return Math.floor((climate.main.temp) - 273.15);
+        }
+        //showing temperature in Celsius
+        temperatureReport.innerHTML = kelvinToCelsius();
+    });
