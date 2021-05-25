52ee0c4e2bc0c9e11d548f1bd387f7b9f25dec37
Parinaz Setayeshgar <p.setayeshgar@yahoo.com>
2021-05-25 16:44:27 +0430

    Adding icon

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-//storing .weather & .temperature elements
+//storing .weather, .temperature elements & #Icon
 var weatherReport = document.querySelector(".weather");
 var temperatureReport = document.querySelector(".temperature");
+var climateIcon = document.querySelector("#icon");
+
 //calling API
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Tehran&units=metric&appid=59047e5b5fb99d3b3a17ffa14fe1730d')
+
     .then(response => response.json()) //creating climate object
+
     .then(function(climate) {
+
         //showing weather description
         weatherReport.innerHTML = climate.weather[0].description;
+
         //showing temperature in Celsius
         temperatureReport.innerHTML = Math.floor(climate.main.temp);
+        
+        //showing icon
+        var iconSrc = "http://openweathermap.org/img/wn/" + climate.weather[0].icon + "@2x.png"
+        climateIcon.setAttribute("src", iconSrc);
     });
